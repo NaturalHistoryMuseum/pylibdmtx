2449cbf718673a78014cf928d6d009f7a1cdeb40
gpongelli <gpongelli@users.noreply.github.com>
2023-01-09 16:40:44 +0100

    align linux load behavior to windows.
    first load shared library, then search local folder

--- pylibdmtx/dmtx_library.py
+++ pylibdmtx/dmtx_library.py
@@ -2,13 +2,15 @@
 """
 import platform
 import sys
-from glob import glob
+import os
+import re
 
 from ctypes import cdll
 from ctypes.util import find_library
 from pathlib import Path
 
 __all__ = ['load']
+_lib_name = 'dmtx'
 
 
 def _windows_fname():
@@ -17,7 +19,7 @@ def _windows_fname():
 
     This logic has its own function to make testing easier
     """
-    return 'libdmtx-64.dll' if sys.maxsize > 2**32 else 'libdmtx-32.dll'
+    return f'lib{_lib_name}-64.dll' if sys.maxsize > 2**32 else f'lib{_lib_name}-32.dll'
 
 
 def load():
@@ -42,14 +44,15 @@ def load():
                 str(Path(__file__).parent.joinpath(fname))
             )
     else:
-        # First search on local folder
-        _lib_search = glob(f"{Path(__file__).parent.joinpath('*dmtx*.so.*.*.*')}", recursive=True)
-        path = None
-        if _lib_search:
-            path = _lib_search[0]
-        else:
-            # Assume a shared library on the path
-            path = find_library('dmtx')
+        # Assume a shared library on the path
+        path = find_library(_lib_name)
+        if not path:
+            # Search on local folder
+            _base_path = Path(__file__).parent.absolute()
+            # linux do put 'lib' as name prefix and can put the version after the extension (e.g. libdmtx.so.0.1.0 )
+            _lib_search = [f for f in os.listdir(_base_path) if re.search(rf'[a-z]*{_lib_name}[a_z]*.so[.0-9]*', f)]
+            if _lib_search:
+                path = _base_path.joinpath(_lib_search[0])
 
         if not path:
             raise ImportError('Unable to find dmtx shared library')
